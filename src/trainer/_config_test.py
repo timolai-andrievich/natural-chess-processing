6d5e23d840dacaf01c5efb2fba22a12ca4faf5ea
timolai-andrievich <timolai.andrievitch@ya.ru>
2023-10-10 15:45:20 +0300

    Added config module

---
+++ src/trainer/_config_test.py
@@ -0,0 +1,63 @@
+"""Module with tests for `layers` module.
+
+Run tests via `pytest src`
+"""
+import os
+
+import pytest
+
+import config
+
+EXAMPLE_CONFIG = {'model': {'name': 'Baseline'},
+                    'dataset': {'name': 'MoveDataset', 'file': 'data/small.txt'},
+                    'optimizer': {'name': 'Adam'},
+                    'scheduler': {'name': 'CosineAnnealingLR'},
+                    'training': {'batch_size': 32, 'epochs': 1000}}
+
+EXAMPLE_CONFIG_TEXT = b"""
+[model]
+name="Baseline"
+
+[dataset]
+name="MoveDataset"
+file="data/small.txt"
+
+[optimizer]
+name="Adam"
+
+[scheduler]
+name="CosineAnnealingLR"
+
+[training]
+batch_size=32
+epochs=1000
+"""
+
+def test_validate_config():
+    """Tests `validate_config` function.
+    """
+    config.validate_config(EXAMPLE_CONFIG)
+    with pytest.raises(config.InvalidConfigError):
+        config.validate_config({})
+
+
+def test_insert_default_values():
+    """Tests `insert_default_values` function.
+    """
+    inputed_config = config.insert_default_values(EXAMPLE_CONFIG)
+    assert inputed_config['model']['params'] == {}
+    assert inputed_config['optimizer']['params'] == {}
+    assert inputed_config['scheduler']['params'] == {}
+
+def test_parse_config():
+    """Tests `parse_config` function.
+    """
+    temp_file_name = 'temp.toml'
+    try:
+        with open(temp_file_name, 'wb') as file:
+            file.write(EXAMPLE_CONFIG_TEXT)
+        cfg = config.parse_config(temp_file_name)
+        assert cfg == EXAMPLE_CONFIG
+    finally:
+        if os.path.exists(temp_file_name):
+            os.remove(temp_file_name)
