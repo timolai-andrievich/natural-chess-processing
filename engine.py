25cde4c2e01cd0a5132072446ec71cc1519ce3e7
timolai-andrievich <timolai.andrievitch@ya.ru>
2023-10-25 14:51:41 +0300

    Added documentation

--- engine.py
+++ engine.py
@@ -1,6 +1,9 @@
-import io
+"""Script implementing part of the UCI (Universal Chess Interface)
+Tested with the "nibbler" GUI.
+"""
+from collections import OrderedDict
 from typing import List
-from sys import stdin, stdout, stderr
+from sys import stdin, stdout
 
 import chess
 import numpy as np
@@ -9,36 +12,55 @@ import torch
 from src.data.dataset import encode_position
 from src.data.vocab import build_vocab
 
-ENGINE_NAME = 'TODO'  # TODO
-ENGINE_AUTHORS = 'TODO'  # TODO
+ENGINE_NAME = 'ncp'
+ENGINE_AUTHORS = 'Timolai Andrievich'
 
 TERMINALS = {'1-0', '0-1', '1/2-1/2'}
 
+OPTIONS = {
+    'ModelFile':
+    OrderedDict(name='ModelFile',
+                type='string',
+                default='models/position/last.ckpt')
+}
+
 
 class NoSuchCommand(Exception):
-    pass
+    """Raised when the command passed to the engine is incorrect.
+    """
 
 
 class PositionEngine:
+    """Simple engine class that works with models that use
+    models that predict next move using position encodings.
+    """
 
     def __init__(self):
+        """Simple engine class that works with models that use
+        models that predict next move using position encodings.
+        """
         self.board = chess.Board()
         self._stop = False
         self._model = None
         self._positions = [encode_position(self.board)]
         self._vocab = build_vocab()
-        # TODO load when option is set
-        self._load_model('models/position/last.ckpt')
+        self._load_model(OPTIONS['ModelFile']['default'])
 
     def _load_model(self, model_path: str):
-        # TODO load weights instead of checkpoint
+        """Loads the model from `model_path` checkpoint.
+
+        Args:
+            model_path (str): Checkpoint file path.
+        """
         self._model = torch.load(model_path, map_location='cpu')
 
     def _get_move(self):
+        """Uses the model to predict the next move.
+        """
         if self._model is None:
             return None, None
         positions = np.array(self._positions)
-        game_tensor = torch.tensor(self._positions)
+        game_tensor = torch.tensor(positions)
         game_tensor = game_tensor.view(1, *game_tensor.shape)
         outputs = self._model(game_tensor)
         move_probs = outputs[0, -1]
@@ -58,6 +80,11 @@ class PositionEngine:
         return bestmove, suggested_result
 
     def go(self, tokens: List[str]):
+        """`go` UCI command.
+
+        Args:
+            tokens (List[str]): List of words that followed the `go` command.
+        """
         move, suggested_result = self._get_move()
         if move is None:
             move = '(none)'
@@ -74,6 +101,12 @@ class PositionEngine:
         stdout.flush()
 
     def position(self, tokens):
+        """`position` UCI command.
+
+        Args:
+            tokens (List[str]): List of words that followed the `position`
+            command.
+        """
         match tokens:
             case 'startpos', :
                 self.board = chess.Board()
@@ -96,19 +129,46 @@ class PositionEngine:
                 raise NoSuchCommand()
 
     def uci(self):
+        """`uci` UCI command.
+        """
         stdout.write(f'id name {ENGINE_NAME}\n')
         stdout.write(f'id author {ENGINE_AUTHORS}\n')
+        for option in OPTIONS.values():
+            stdout.write(
+                f'{" ".join(f"{key} {value}" for key, value in option.items())}\n'
+            )
         stdout.write('uciok\n')
         stdout.flush()
 
     def isready(self):
+        """`isready` UCI command.
+        """
         stdout.write('readyok\n')
         stdout.flush()
 
     def quit(self):
+        """`quit` UCI command.
+        """
         self._stop = True
 
+    def setoption(self, tokens: List[str]):
+        """`setoption` UCI command.
+
+        Args:
+            tokens (List[str]): List of words that followed the `setoption` command.
+        """
+        name_index = tokens.index('name')
+        value_index = tokens.index('value')
+        name = tokens[name_index + 1]
+        value = tokens[value_index + 1]
+        match name:
+            case 'ModelFile':
+                self._load_model(value)
+
     def mainloop(self):
+        """The main loop of the engine. Reads commands from the `stdin`,
+        writes commands to `stdout`.
+        """
         while not self._stop:
             line = stdin.readline()
             command = line.strip()
@@ -128,19 +188,21 @@ class PositionEngine:
                     case 'position', *tokens:
                         self.position(tokens)
                     case 'setoption', *tokens:
-                        pass
+                        self.setoption(tokens)
                     case 'stop', :
                         self.go([])
                     case 'ucinewgame', :
                         pass
                     case _otherwise:
                         raise NoSuchCommand()
-            except Exception as e:
+            except Exception:
                 stdout.write(f'No such command: {command}\n')
                 stdout.flush()
 
 
 def main():
+    """Main function of the script.
+    """
     engine = PositionEngine()
     engine.mainloop()
 
